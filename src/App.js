4367b790cc200b8bb6174dbeefdb344fe0fd565d
Jessica Buckwalter <calico2021@gmail.com>
2025-05-08 08:38:20 -0500

    Title change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          LETS DO THIS!!!
+          LETS MAKE THIS SAPPY APP ;D
         </p>
         <a
           className="App-link"
