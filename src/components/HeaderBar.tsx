1dc7b2c253aabc136c4a32877cd2a7736281e8c7
Jessica Buckwalter <calico2021@gmail.com>
2025-05-11 18:27:41 -0500

    Create Header

---
+++ src/components/HeaderBar.tsx
@@ -0,0 +1,28 @@
+import { Dropdown } from 'react-bootstrap'
+import React from 'react'
+import { Navbar, Container } from 'react-bootstrap'
+
+function App() {
+  return (
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Sappy SpellBook</Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+          <Dropdown drop="start">
+            <Dropdown.Toggle bsPrefix="custom-dropdown-toggle">
+              <span className="text-white" style={{ fontSize: '24px', lineHeight: '1' }}>⋮</span>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#action1">Settings</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  )
+}
+
+export default App
