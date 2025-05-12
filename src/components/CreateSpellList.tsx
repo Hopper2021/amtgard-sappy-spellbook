bdf1e68e0ea68d1f01bf2cc1ced3711330d7a9c1
Jessica Buckwalter <calico2021@gmail.com>
2025-05-11 19:38:48 -0500

    use Navigate to connect floating action button and create spell list page

---
+++ src/components/CreateSpellList.tsx
@@ -0,0 +1,16 @@
+import { Container, Col, Dropdown, Row } from 'react-bootstrap'
+import React from 'react'
+
+function CreateSpellList() {
+  return (
+    <>
+        <Container>
+            <Row>
+                <Col>I'M A COLUMN IN A ROW</Col>
+            </Row>
+        </Container>
+    </>
+  )
+}
+
+export default CreateSpellList
--- src/components/FloatingActionButton.tsx
+++ src/components/FloatingActionButton.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import { LuCirclePlus } from "react-icons/lu";
+import { useNavigate } from 'react-router-dom';
 
 function FloatingActionButton() {
+  const navigate = useNavigate()
+
   return (
     <Button
         variant="dark"
         className="position-fixed bottom-0 end-0 m-4 rounded-circle d-flex justify-content-center align-items-center"
         style={{ width: '60px', height: '60px' }}
+        onClick={() => navigate('/createList')}
     >
         <LuCirclePlus size={22} color="white" />
     </Button>
