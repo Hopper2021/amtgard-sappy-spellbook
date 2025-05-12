a078c2db533309680d62b932b667765e9b749a5c
Jessica Buckwalter <calico2021@gmail.com>
2025-05-11 22:41:30 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HeaderBar from './components/HeaderBar.tsx'
 import Home from './components/Home.tsx'
 import { Route, Routes } from 'react-router-dom'
 import CreateSpellList from './components/CreateSpellList.tsx'
+import SpellListDetails from './components/SpellListDetails.tsx'
+import EditSpellList from './components/EditSpellList.tsx'
 
 function App() {
   return (
@@ -11,8 +13,9 @@ function App() {
       <HeaderBar />
         <Routes>
           <Route path="/" element={<Home />}/>
-          <Route path="/listDetails" />
+          <Route path="/listDetails" element={<SpellListDetails />}/>
           <Route path="/createList" element={<CreateSpellList />}/>
+          <Route path="/editList" element={<EditSpellList />}/>
         </Routes>
     </>
   )
