3bf78aaaa30173f8002fd3f5806475faf4fa4233
Jessica Buckwalter <calico2021@gmail.com>
2025-05-11 19:22:18 -0500

    Install icon for action button

--- src/components/FloatingActionButton.tsx
+++ src/components/FloatingActionButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
+import { LuCirclePlus } from "react-icons/lu";
 
 function FloatingActionButton() {
   return (
@@ -8,7 +9,7 @@ function FloatingActionButton() {
         className="position-fixed bottom-0 end-0 m-4 rounded-circle"
         style={{ width: '60px', height: '60px' }}
     >
-        +
+        <LuCirclePlus size={30} color="white" />
     </Button>
   )
 }
