750a1e446afce79bf9f50a33fdcff6e67ae32cc5
Jessica Buckwalter <calico2021@gmail.com>
2025-05-11 19:15:06 -0500

    Create Routes

--- src/App.js
+++ src/App.js
@@ -1,47 +1,18 @@
 import './App.css'
-import Header from '../src/components/HeaderBar.tsx'
-import FloatingActionButton from './components/FloatingActionButton.tsx'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from 'react'
+import HeaderBar from './components/HeaderBar.tsx'
+import Home from './components/Home.tsx'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  const mockSpellListArray = [
-    {
-      id: 1,
-      name: 'Test Healer List',
-      class: 'Healer',
-      maxLevel: 6,
-      spells: []
-    },
-    {
-      id: 1,
-      name: 'Test Bard List',
-      class: 'Bard',
-      maxLevel: 3,
-      spells: []
-    },
-    {
-      id: 1,
-      name: 'Test Druid List',
-      class: 'Druid',
-      maxLevel: 4,
-      spells: []
-    },
-  ]
-
   return (
     <>
-      <Header />
-      <Container fluid className="p-3">
-          <h6>Spell Books</h6>
-          <Container className="px-4 pt-1">
-            {mockSpellListArray.map((spellList) => (
-              <Row key={spellList.id} className="border-bottom mb-3">
-                <Col className="p-1">{spellList.name} ( {spellList.class}, level {spellList.maxLevel} )</Col>
-              </Row>
-            ))}
-          </Container>
-      </Container>
-      <FloatingActionButton />
+      <HeaderBar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/listDetails" />
+          <Route path="/createList" />
+        </Routes>
     </>
   )
 }
