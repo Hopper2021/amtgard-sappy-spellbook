b8f6d9817f1b0d33fdf5efaab9d4cff87a039e6e
Jessica Buckwalter <calico2021@gmail.com>
2025-05-11 22:06:14 -0500

    Create spell list details page

---
+++ src/components/SpellListDetails.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Container, Row, Button, Col, Form } from 'react-bootstrap'
+
+function SpellListDetails() {
+  const mockPropsSpellList = {
+    id: 1,
+    name: 'Test Healer List',
+    class: 'Healer',
+    maxLevel: 6,
+    spells: []
+  }
+
+  return (
+    <Container className="pt-3">
+      <Row className="d-flex">
+        <Col xs="auto" className="pr-0">
+          <h4>Overview</h4>
+        </Col>
+        <Col className="flex-grow-1">
+          <Form.Check type={'checkbox'} label={'show type/school'} />
+          <Form.Check type={'checkbox'} label={'show incantation'} />
+          <Form.Check type={'checkbox'} label={'show strips'} />
+        </Col>
+        <Col xs="auto">
+          <Button className="ms-auto d-block">Edit</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Form.Text>Class: {mockPropsSpellList.class}</Form.Text>
+        <Form.Text>Points Remaining...</Form.Text>
+        <Form.Text>L1: 5, L2: 5, L3: 5, L4: 5, L5: 5, L6: 6</Form.Text>
+        <Form.Text>**** Spells ****</Form.Text>
+        <Form.Text>Level 1</Form.Text>
+        <Form.Text>Level 2</Form.Text>
+        <Form.Text>Level 3</Form.Text>
+        <Form.Text>Level 4</Form.Text>
+        <Form.Text>Level 5</Form.Text>
+        <Form.Text>Level 6</Form.Text>
+      </Row>
+    </Container>
+  )
+}
+
+export default SpellListDetails
