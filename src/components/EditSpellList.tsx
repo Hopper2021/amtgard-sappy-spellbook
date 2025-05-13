d5747bcfc412b90f2efeee5829ec65ac580131ba
Jessica Buckwalter <calico2021@gmail.com>
2025-05-12 23:09:25 -0500

    Refined mock data and added functions to calculate the data displayed

--- src/components/EditSpellList.tsx
+++ src/components/EditSpellList.tsx
@@ -1,104 +1,99 @@
 import React from 'react'
-import { Container, Row, Accordion, Form } from 'react-bootstrap'
+import { Container, Row, Accordion, Button } from 'react-bootstrap'
+import { ALL_SPELLS, BARD_SPELLS } from '../appConstants'
 
 function EditSpellList() {
-  const mockPropsSpellList = {
+  // TODO: Add Remove button and update state
+  // Spell list will change to only show the spells in the local storage list and clicking them will remove them instead
+  const [addOrRemoveSpells, setAddOrRemoveSpells] = React.useState('Add')
+
+  // TODO: This should be taken from local storage
+  // On Create page, this object will be set
+  // On This edit page, this object should be updated every time a spell is added or removed
+  // This should minimize data loss
+  const mockSpellList = {
     id: 1,
-    name: 'Test Healer List',
-    class: 'Healer',
+    name: 'Test Bard List',
+    class: 'Bard',
     maxLevel: 6,
-    spells: []
+    lookThePart: true,
+    spellsByLevel: [
+      { level: 1, points: 5, spells: [ { id: 52, purchased: 1 }] },
+      { level: 2, points: 5, spells: [ { id: 46, purchased: 1 }] },
+      { level: 3, points: 5, spells: [ { id: 20, purchased: 1 }] },
+      { level: 4, points: 5, spells: [ { id: 5, purchased: 1 }] },
+      { level: 5, points: 5, spells: [ { id: 8, purchased: 1 }] },
+      { level: 6, points: 6, spells: [ { id: 29, purchased: 1 }] },
+    ]
   }
 
-const BARD_SPELLS = [
-  {
-    level: 1,
-    spells: [
-      { id: 33, cost: 1, max: null, frequency: '1/Refresh Charge x5'},
-      { id: 52, cost: 2, max: 2, frequency: null},
-      { id: 56, cost: 2, max: 2, frequency: null},
-      { id: 88, cost: 1, max: null, frequency: '1/Life'},
-      { id: 124, cost: 1, max: null, frequency: '1/Life'},
-      { id: 134, cost: 1, max: null, frequency: '1/Life'},
-      { id: 142, cost: 1, max: 1, frequency: 'Unlimited'},
-    ]
-  },
-  {
-    level: 2,
-    spells: [
-      { id: 46, cost: 1, max: null, frequency: '2/Refresh'},
-      { id: 49, cost: 3, max: 1, frequency: null},
-      { id: 76, cost: 2, max: null, frequency: '1/Refresh'},
-      { id: 86, cost: 1, max: 4, frequency: '1/Refresh'},
-      { id: 98, cost: 1, max: null, frequency: '1/Life'},
-      { id: 140, cost: 1, max: 1, frequency: 'Unlimited'},
-      { id: 147, cost: 1, max: 1, frequency: 'Unlimited'},
-    ]
-  },
-  {
-    level: 3,
-    spells: [
-      { id: 20, cost: 1, max: null, frequency: '1/Life'},
-      { id: 15, cost: 1, max: 1, frequency: '1/Refresh'},
-      { id: 31, cost: 1, max: 4, frequency: '1/Life'},
-      { id: 58, cost: 1, max: 2, frequency: '1/Life'},
-      { id: 51, cost: 2, max: 1, frequency: null},
-      { id: 143, cost: 1, max: 1, frequency: 'Unlimited'},
-    ]
-  },
-  {
-    level: 4,
-    spells: [
-      { id: 5, cost: 1, max: 4, frequency: '1/Life'},
-      { id: 175, cost: 3, max: 1, frequency: null},
-      { id: 126, cost: 1, max: null, frequency: '1/Refresh'},
-      { id: 136, cost: 1, max: null, frequency: '1/Refresh'},
-      { id: 141, cost: 1, max: 1, frequency: 'Unlimited'},
-      { id: 145, cost: 1, max: 1, frequency: 'Unlimited'},
-      { id: 156, cost: 1, max: null, frequency: '1/Refresh'},
-      { id: 159, cost: 1, max: 2, frequency: '1/Life'},
-      { id: 161, cost: 1, max: null, frequency: '1/Refresh'},
-    ]
-  },
-  {
-    level: 5,
-    spells: [
-      { id: 8, cost: 1, max: null, frequency: '1/Refresh'},
-      { id: 6, cost: 1, max: 2, frequency: '1/Life'},
-      { id: 42, cost: 1, max: 1, frequency: '1/Refresh'},
-      { id: 50, cost: 3, max: 1, frequency: null},
-      { id: 77, cost: 1, max: null, frequency: '1/Refresh'},
-      { id: 93, cost: 1, max: null, frequency: '1/Life'},
-      { id: 146, cost: 1, max: 1, frequency: 'Unlimited'},
-    ]
-  },
-  {
-    level: 6,
-    spells: [
-      { id: 29, cost: 2, max: 1, frequency: null},
-      { id: 40, cost: 2, max: 1, frequency: null},
-      { id: 49, cost: 2, max: 1, frequency: null},
-      { id: 91, cost: 1, max: 1, frequency: null},
-      { id: 135, cost: 1, max: null, frequency: '1/Refresh'},
-      { id: 144, cost: 1, max: 1, frequency: '1/Refresh Charge x5'},
-      { id: 148, cost: 1, max: null, frequency: '1/Refresh'},
-    ]
-  },
-]
+  const getSpellDetails = (spellId: number) => {
+    const spell = ALL_SPELLS.find(spell => spell.id === spellId)
+    if (spell) {
+      return spell.name
+    }
+    return null
+  }
+
+  const calculateLevelPointsAvailable = (level: number) => {
+    const listLevel = mockSpellList.spellsByLevel.find(listLevel => listLevel.level === level)
+    if (listLevel) {
+      return listLevel.points
+    }
+    return null
+  }
+
+  const calculateTrickleDownPointsAvailable = (level: number) => {
+    const levelsToSum = mockSpellList.spellsByLevel.filter((listLevel) => listLevel.level >= level)
+    const totalPoints = levelsToSum.reduce((sum, listLevel) => sum + listLevel.points, 0)
+
+    return totalPoints
+  }
+
+  const getAmountPurchased = (spellId) => {
+    for (const level of mockSpellList.spellsByLevel) {
+      const spell = level.spells.find((spell) => spell.id === spellId)
+      if (spell) {
+        return `x${spell.purchased}`
+      }
+    }
+    return ''
+  }
+
+  const calculateAmountPurchased = (spellId) => {
+    for (const level of mockSpellList.spellsByLevel) {
+      const spell = level.spells.find((spell) => spell.id === spellId)
+      if (spell && addOrRemoveSpells === 'Add') {
+        return `x${spell.purchased + 1}`
+        // TO DO: Plan is to update the local storage every time a spell is added or removed
+        // Not sure if this can be done in local state. Might lose data if you navigate away
+      }
+    }
+    return ''
+  }
+
+  console.log('mockSpellList', mockSpellList)
 
   return (
     <Container fluid className="p-3">
-        <h6>Edit {mockPropsSpellList.class} Spells</h6>
+        <h6>Edit {mockSpellList.class} Spells</h6>
         <Container>
-          {mockHealerSpells.map((level) => (
+          {BARD_SPELLS.map((level) => (
             <Accordion defaultActiveKey="1" flush>
               <Accordion.Item eventKey="0" className="border-bottom">
-                <Accordion.Header>Level {level.level}  ({level.pointsAvailable}) available: {level.pointsAvailable}</Accordion.Header>
-                {/* put function above to calculate available points based on level passed in and points spent in current level and higher*/}
+                <Accordion.Header>
+                  Level {level.level}  ({calculateLevelPointsAvailable(level.level)}) available: {calculateTrickleDownPointsAvailable(level.level)}
+                </Accordion.Header>
                 <Accordion.Body>
-                  {level.spells.map((spell) => (
+                  {level.spells.map((spellsByLevel) => (
                     <Row className="d-flex justify-content-between">
-                      <Form.Text>{spell.name} (cost: {spell.cost})</Form.Text>
+                      <Button
+                        variant="unknown"
+                        className="text-start border-bottom"
+                        onClick={() => calculateAmountPurchased(spellsByLevel.id)}
+                      >
+                        {getSpellDetails(spellsByLevel.id)} {getAmountPurchased(spellsByLevel.id)} (cost: {spellsByLevel.cost})
+                      </Button>
                     </Row>
                   ))}
                 </Accordion.Body>
