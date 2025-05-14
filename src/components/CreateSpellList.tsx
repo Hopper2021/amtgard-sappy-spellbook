9b1839fe532b26a62dac07a413007d2585757032
Jessica Buckwalter <calico2021@gmail.com>
2025-05-13 21:59:44 -0500

    Set new spell list to local state

--- src/components/CreateSpellList.tsx
+++ src/components/CreateSpellList.tsx
@@ -2,10 +2,50 @@ import React from 'react'
 import { Button, Col, Container, Dropdown, Row } from 'react-bootstrap'
 import Form from 'react-bootstrap/Form'
 import InputGroup from 'react-bootstrap/InputGroup'
+import { useNavigate } from 'react-router-dom'
+
+interface Spell {
+	id: number
+	name: string
+	level: number
+	description: string
+}
+
+interface SpellList {
+	id: number
+	name: string
+	class: string
+	maxLevel: number
+	lookThePart: boolean
+	spells: Spell[]
+}
 
 function CreateSpellList() {
-  return (
-    <Container fluid className="p-2">
+	const navigate = useNavigate()
+
+	if (localStorage.getItem('allSpellLists') === null) {
+		localStorage.setItem('allSpellLists', JSON.stringify([]))
+	}
+
+	const allSpellLists = JSON.parse(localStorage.getItem('allSpellLists') || '[]')
+
+	const generateNewId = () => {
+		const maxId = allSpellLists.reduce((max, spellList) => Math.max(max, spellList.id), 0)
+		console.log('maxId', maxId + 1)
+		return maxId + 1
+	}
+
+	const [newSpellList, setNewSpellList] = React.useState<SpellList>({
+		id: generateNewId(),
+		name: 'My SpellBook',
+		class: 'Bard',
+		maxLevel: 1,
+		lookThePart: false,
+		spells: [],
+	})
+
+	return (
+		<Container fluid className="p-2">
 			<Row className="align-items-center">
 				<Col xs={7}>
 					<InputGroup style={{ border: 'none' }} className="mb-3">
@@ -21,6 +61,9 @@ function CreateSpellList() {
 								borderBottom: '1px solid black',
 								borderRadius: 0,
 							}}
+							onChange={(event) =>
+								setNewSpellList({...newSpellList, name: event.target.value })
+							}
 						/>
 					</InputGroup>
 				</Col>
@@ -46,43 +89,73 @@ function CreateSpellList() {
 							</Dropdown.Toggle>
 
 							<Dropdown.Menu>
-								<Dropdown.Item onClick={() => console.log(1)}>1</Dropdown.Item>
-								<Dropdown.Item onClick={() => console.log(2)}>2</Dropdown.Item>
-								<Dropdown.Item onClick={() => console.log(3)}>3</Dropdown.Item>
-								<Dropdown.Item onClick={() => console.log(4)}>4</Dropdown.Item>
-								<Dropdown.Item onClick={() => console.log(5)}>5</Dropdown.Item>
-								<Dropdown.Item onClick={() => console.log(6)}>6</Dropdown.Item>
+								<Dropdown.Item onClick={() => setNewSpellList({ ...newSpellList, maxLevel: 1 })}>1</Dropdown.Item>
+								<Dropdown.Item onClick={() => setNewSpellList({ ...newSpellList, maxLevel: 2 })}>2</Dropdown.Item>
+								<Dropdown.Item onClick={() => setNewSpellList({ ...newSpellList, maxLevel: 3 })}>3</Dropdown.Item>
+								<Dropdown.Item onClick={() => setNewSpellList({ ...newSpellList, maxLevel: 4 })}>4</Dropdown.Item>
+								<Dropdown.Item onClick={() => setNewSpellList({ ...newSpellList, maxLevel: 5 })}>5</Dropdown.Item>
+								<Dropdown.Item onClick={() => setNewSpellList({ ...newSpellList, maxLevel: 6 })}>6</Dropdown.Item>
 							</Dropdown.Menu>
 						</Dropdown>
 					</InputGroup>
 				</Col>
 			</Row>
-      
+
 			<InputGroup className="mb-3">
 				<InputGroup.Text style={{ backgroundColor: 'transparent', border: 'none', paddingRight: 5 }}>
-					Class: 
+					Class:
 				</InputGroup.Text>
 				<Dropdown>
-					<Dropdown.Toggle style={{ backgroundColor: 'transparent', color: 'black', border: 'none', borderBottom: '1px solid black', borderRadius: 0 }} id="dropdown-basic">
+					<Dropdown.Toggle
+						style={{
+							backgroundColor: 'transparent',
+							color: 'black',
+							border: 'none',
+							borderBottom: '1px solid black',
+							borderRadius: 0,
+						}}
+						id="dropdown-basic"
+					>
 						Bard
 					</Dropdown.Toggle>
 
 					<Dropdown.Menu>
-						<Dropdown.Item onClick={() => console.log('Bard')}>Bard</Dropdown.Item>
-						<Dropdown.Item onClick={() => console.log('Druid')}>Druid</Dropdown.Item>
-						<Dropdown.Item onClick={() => console.log('Healer')}>Healer</Dropdown.Item>
-						<Dropdown.Item onClick={() => console.log('Wizard')}>Wizard</Dropdown.Item>
+						<Dropdown.Item onClick={() => setNewSpellList({ ...newSpellList, class: 'Bard' })}>Bard</Dropdown.Item>
+						<Dropdown.Item onClick={() => setNewSpellList({ ...newSpellList, class: 'Druid' })}>Druid</Dropdown.Item>
+						<Dropdown.Item onClick={() => setNewSpellList({ ...newSpellList, class: 'Healer' })}>Healer</Dropdown.Item>
+						<Dropdown.Item onClick={() => setNewSpellList({ ...newSpellList, class: 'Wizard' })}>Wizard</Dropdown.Item>
 					</Dropdown.Menu>
 				</Dropdown>
 			</InputGroup>
-			
+
 			<InputGroup className="mb-3">
-				<Form.Check className="ml-2" type={'checkbox'} label={'Look The Part'} />
+				<Form.Check
+					className="ml-2"
+					type={'checkbox'}
+					label={'Look The Part'}
+					onClick={() =>
+						setNewSpellList({
+							...newSpellList,
+							lookThePart: !newSpellList.lookThePart,
+						})
+					}
+				/>
 			</InputGroup>
 
-      <Button className="w-100" variant="unknown" style={{ backgroundColor: "lightgrey" }}>CREATE</Button>
-    </Container>
-  )
+			<Button
+				className="w-100"
+				variant="unknown"
+				style={{ backgroundColor: 'lightgrey' }}
+				onClick={() => {
+					const updatedAllSpellLists = [...allSpellLists, newSpellList]
+					localStorage.setItem('allSpellLists', JSON.stringify(updatedAllSpellLists))
+					navigate('/')
+				}}
+			>
+				CREATE
+			</Button>
+		</Container>
+	)
 }
 
 export default CreateSpellList
