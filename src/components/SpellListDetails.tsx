735dcde8928e2039c03c619e5e3fcbeb06ce7ad9
Jessica Buckwalter <calico2021@gmail.com>
2025-05-15 23:16:28 -0500

    Use real data and provide styling for when all checkmarks are checked and list contains a lot of text

--- src/components/SpellListDetails.tsx
+++ src/components/SpellListDetails.tsx
@@ -26,15 +26,10 @@ function SpellListDetails() {
   const [showStrips, setShowStrips] = React.useState(false)
 
   const allSpellLists = JSON.parse(localStorage.getItem('allSpellLists') || '[]')
-  // const spellList = allSpellLists.find((list: SpellList) => list.id === parseInt(id || '0'))
-
-  // console.log('spellList', spellList)
+  const spellList = allSpellLists.find((list: SpellList) => list.id === parseInt(id || '0'))
 
   const fetchSpellDetails = (key: string, spellId: number) => {
     const spell = ALL_SPELLS.find(spell => spell.id === spellId)
-
-    console.log('spell', spell)
-    console.log('key', key)
 
     if (key === 'name') {
       return spell?.name
@@ -57,46 +52,6 @@ function SpellListDetails() {
     } else if (key === 'materials') {
       return spell?.materials
     }
-  }
-
-  const spellList = {
-    id: 1,
-    name: 'Mock Spell list',
-    class: 'Bard',
-    maxLevel: 6,
-    lookThePart: true,
-    spells: [
-      {
-          level: 1,
-          points: 5,
-          spells: [{ id: 1, purchased: 1 }, { id: 2, purchased: 2 }, { id: 3, purchased: 3 }],
-      },
-      {
-          level: 2,
-          points: 5,
-          spells: [{ id: 4, purchased: 1 }, { id: 5, purchased: 2 }, { id: 6, purchased: 3 }],
-      },
-      {
-          level: 3,
-          points: 3,
-          spells: [{ id: 7, purchased: 1 }, { id: 8, purchased: 2 }, { id: 9, purchased: 3 }],
-      },
-      {
-          level: 4,
-          points: 5,
-          spells: [{ id: 10, purchased: 1 }, { id: 11, purchased: 2 }, { id: 12, purchased: 3 }],
-      },
-      {
-          level: 5,
-          points: 5,
-          spells: [{ id: 13, purchased: 1 }, { id: 14, purchased: 2 }, { id: 15, purchased: 3 }],
-      },
-      {
-          level: 6,
-          points: 6,
-          spells: [{ id: 16, purchased: 1 }, { id: 17, purchased: 2 }, { id: 18, purchased: 3 }],
-      }
-    ]
   }
 
   return (
@@ -146,7 +101,7 @@ function SpellListDetails() {
         <Col xs="auto">
           <Button
             className="ms-auto d-block"
-            onClick={() => navigate('/editList')}>
+            onClick={() => navigate(`/editList/${spellList.id}`)}>
               Edit
           </Button>
         </Col>
@@ -161,7 +116,7 @@ function SpellListDetails() {
                 key={index}
                 className="me-3">
                   L{level.level}: {level.points},
-              </Form.Text> // When points are 0 for a level, do not show this Form Text. When All points are spent in all levels, show "All points spent".
+              </Form.Text>
             )
           )}
         </div>
@@ -172,16 +127,21 @@ function SpellListDetails() {
             {level.spells.map((spell, spellIdx) => (
               <Row key={spellIdx} className="m-0">
                 <div>
-                  {spell.purchased}x {fetchSpellDetails('name', spell.id) || 'Unknown Spell'}{' '}
+                  {spell.purchased}x{' '}
+                  <span style={{ textDecoration: 'underline' }}>
+                    {fetchSpellDetails('name', spell.id) || 'Unknown Spell'}
+                  </span>{' '}
                   {showTypeAndSchool && <span>( {fetchSpellDetails('type', spell.id)} )</span>}
                   {fetchSpellDetails('school', spell.id) && showTypeAndSchool && (
                     <span>( {fetchSpellDetails('school', spell.id)} )</span>
                   )}
                   <div className="m-0">
-                    {showIncantation && <span>{fetchSpellDetails('incantation', spell.id)}</span>}
+                    {showIncantation && <span style={{ fontStyle: 'italic' }}>{fetchSpellDetails('incantation', spell.id)}</span>}
                   </div>
                   <div className="m-0">
-                    {showIncantation && <span>{fetchSpellDetails('materials', spell.id)}</span>}
+                      {showStrips && fetchSpellDetails('materials', spell.id) ? (
+                        <span>( {fetchSpellDetails('materials', spell.id)} )</span>
+                      ) : null}
                     {/* Consider revisiting how this looks. Magic balls have the ball displayed twice in two places. Maybe remove Green Magic Ball from materials? */}
                   </div>
                 </div>
