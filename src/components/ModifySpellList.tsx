1af9ca005f5a1c6bc25be55b0967a7456ae07c74
Jessica Buckwalter <calico2021@gmail.com>
2025-05-18 22:52:21 -0500

    Disable class change in modify list

--- src/components/ModifySpellList.tsx
+++ src/components/ModifySpellList.tsx
@@ -118,17 +118,11 @@ function ModifySpellList() {
 							borderBottom: '1px solid black',
 							borderRadius: 0,
 						}}
+						disabled
 						id="dropdown-basic"
 					>
 						{modifiedSpellList.class}
 					</Dropdown.Toggle>
-
-					<Dropdown.Menu>
-						<Dropdown.Item onClick={() => setModifiedSpellList({ ...modifiedSpellList, class: 'Bard' })}>Bard</Dropdown.Item>
-						<Dropdown.Item onClick={() => setModifiedSpellList({ ...modifiedSpellList, class: 'Druid' })}>Druid</Dropdown.Item>
-						<Dropdown.Item onClick={() => setModifiedSpellList({ ...modifiedSpellList, class: 'Healer' })}>Healer</Dropdown.Item>
-						<Dropdown.Item onClick={() => setModifiedSpellList({ ...modifiedSpellList, class: 'Wizard' })}>Wizard</Dropdown.Item>
-					</Dropdown.Menu>
 				</Dropdown>
 			</InputGroup>
 
