6087df9422f7c26accb7ccfb9f77e686eed5620b
Hopper2021 <jessica.a.buckwalter@gmail.com>
2025-06-03 00:04:41 -0500

    Add all other subclass conditional equipment changes

--- src/components/SpellListDetails.tsx
+++ src/components/SpellListDetails.tsx
@@ -18,6 +18,27 @@ import {
   ANTIPALADIN_EQUIPMENT,
   INFERNAL_SPELLS,
   CORRUPTOR_SPELLS,
+  ARCHER_EQUIPMENT,
+  ASSASSIN_EQUIPMENT,
+  BARBARIAN_EQUIPMENT,
+  MONK_EQUIPMENT,
+  PALADIN_EQUIPMENT,
+  SCOUT_EQUIPMENT,
+  WARRIOR_EQUIPMENT,
+  SNIPER_SPELLS,
+  ARTIFICER_SPELLS,
+  RUFFIAN_SPELLS,
+  SPY_SPELLS,
+  RAIDER_SPELLS,
+  BERSERKER_SPELLS,
+  MEDIUM_SPELLS,
+  MYSTIC_SPELLS,
+  GUARDIAN_SPELLS,
+  INQUISITOR_SPELLS,
+  HUNTER_SPELLS,
+  APEX_SPELLS,
+  MARAUDER_SPELLS,
+  JUGGERNAUT_SPELLS,
 } from '../appConstants'
 
 interface SpellLevel {
@@ -54,6 +75,30 @@ function SpellListDetails() {
   const spellList = allSpellLists.find((list: SpellList) => list.id === parseInt(id || '0'))
   const allPointsSpent = spellList.spells.every(level => level.points === 0)
 
+
+  const isArchetypeChosen = (spellList: SpellList, archetypeId: number): boolean => {
+    return spellList.spells.some(level =>
+      level.spells.some(baseObj =>
+        baseObj.optionalPickOne &&
+        baseObj.optionalPickOne.some(
+          spell => spell.id === archetypeId && spell.chosen === true
+        )
+      )
+    )
+  }
+  // martial subclass Checks
+  const isInfernal = isArchetypeChosen(spellList, 85)
+  const isCorruptor = isArchetypeChosen(spellList, 36)
+  const isArtificer = isArchetypeChosen(spellList, 10)
+  const isRuffian = isArchetypeChosen(spellList, 181)
+  const isSpy = isArchetypeChosen(spellList, 150)
+  const isRaider = isArchetypeChosen(spellList, 117)
+  const isBerserker = isArchetypeChosen(spellList, 22)
+  const isMedium = isArchetypeChosen(spellList, 97)
+  const isMystic = isArchetypeChosen(spellList, 102)
+  const isHunter = isArchetypeChosen(spellList, 80)
+  const isMarauder = isArchetypeChosen(spellList, 178)
+
   const spellsByClass =
     (spellList?.class === 'Bard' && BARD_SPELLS) ||
     (spellList?.class === 'Healer' && HEALER_SPELLS) ||
@@ -68,19 +113,24 @@ function SpellListDetails() {
     (spellList?.class === 'Scout' && SCOUT_LIST) ||
     (spellList?.class === 'Warrior' && WARRIOR_LIST) ||
     []
-  
-  const equipmentByClass: EquipmentByClass =
-    (spellList?.class === 'Anti-Paladin' && ANTIPALADIN_EQUIPMENT) || {
-      lookThePart: '',
-      armor: '',
-      shields: '',
-      weapons: '',
-    }
 
   const subclassSpellsMap = {
     "Infernal": INFERNAL_SPELLS,
     "Corruptor": CORRUPTOR_SPELLS,
-    // Add other subclasses here
+    "Sniper": SNIPER_SPELLS,
+    "Artificer": ARTIFICER_SPELLS,
+    "Ruffian": RUFFIAN_SPELLS,
+    "Spy": SPY_SPELLS,
+    "Raider": RAIDER_SPELLS,
+    "Berserker": BERSERKER_SPELLS,
+    "Medium": MEDIUM_SPELLS,
+    "Mystic": MYSTIC_SPELLS,
+    "Guardian": GUARDIAN_SPELLS,
+    "Inquisitor": INQUISITOR_SPELLS,
+    "Hunter": HUNTER_SPELLS,
+    "Apex": APEX_SPELLS,
+    "Marauder": MARAUDER_SPELLS,
+    "Juggernaut": JUGGERNAUT_SPELLS,
   }
 
   const martialClasses = [
@@ -121,6 +171,164 @@ function SpellListDetails() {
       return spell?.materials
     }
   }
+
+  const fetchSubclassSpellDetails = (key: string, spellId: number, chosenArchetype: string) => {
+    const subclassSpells = subclassSpellsMap[chosenArchetype || ''] || []
+    const spell = subclassSpells.find(s => s.id === spellId)
+
+    if (key === 'magical') {
+      return spell?.magical
+    } else if (key === 'ambulant') {
+      return spell?.ambulant
+    } else if (key === 'extraordinary') {
+      return spell?.extraordinary
+    } else if (key === 'trait') {
+      return spell?.trait
+    } else if (key === 'swift') {
+      return spell?.swift
+    }
+  }
+
+  const fetchEquipmentChanges = (spellList: SpellList) => {
+    let equipmentByClass: EquipmentByClass =
+      (spellList?.class === 'Anti-Paladin' && ANTIPALADIN_EQUIPMENT) ||
+      (spellList?.class === 'Archer' && ARCHER_EQUIPMENT) ||
+      (spellList?.class === 'Assassin' && ASSASSIN_EQUIPMENT) ||
+      (spellList?.class === 'Barbarian' && BARBARIAN_EQUIPMENT) ||
+      (spellList?.class === 'Monk' && MONK_EQUIPMENT) ||
+      (spellList?.class === 'Paladin' && PALADIN_EQUIPMENT) || 
+      (spellList?.class === 'Scout' && SCOUT_EQUIPMENT) || 
+      (spellList?.class === 'Warrior' && WARRIOR_EQUIPMENT) || {
+        lookThePart: '',
+        armor: '',
+        shields: '',
+        weapons: '',
+      }
+
+    let weaponsColor = ''
+    let shieldsColor = ''
+    let armorColor = ''
+    let lookThePartColor = ''
+
+    // Anti-Paladin
+    if (spellList.class === 'Anti-Paladin') {
+      if (isInfernal) {
+        equipmentByClass = {
+          ...equipmentByClass,
+          shields: 'May not wield shields due to Infernal',
+        }
+        shieldsColor = 'red'
+      } else if (isCorruptor) {
+        equipmentByClass = {
+          ...equipmentByClass,
+          lookThePart: 'Terror 1/Life Charge x10 (m)',
+          weapons: 'No Javelins or Great Weapons due to Corruptor',
+        }
+        weaponsColor = 'red'
+        lookThePartColor = 'green'
+      }
+    }
+    // Archer
+    if (spellList.class === 'Archer') {
+      if (isArtificer) {
+        equipmentByClass = {
+          ...equipmentByClass,
+          shields: 'Small shield due to Artificer',
+        }
+        shieldsColor = 'green'
+      }
+    }
+    // Assassin
+    if (spellList.class === 'Assassin') {
+      if (isRuffian) {
+        equipmentByClass = {
+          ...equipmentByClass,
+          weapons: 'No long weapons or bows due to Ruffian',
+        }
+        weaponsColor = 'red'
+      }
+      if (isSpy) {
+        equipmentByClass = {
+          ...equipmentByClass,
+          armor: 'No armor due to Spy',
+        }
+        armorColor = 'red'
+      }
+    }
+    // Barbarian
+    if (spellList.class === 'Barbarian') {
+      if (isRaider) {
+        equipmentByClass = {
+          ...equipmentByClass,
+          shields: 'None due to Raider',
+        }
+        shieldsColor = 'red'
+      }
+      if (isBerserker) {
+        equipmentByClass = {
+          ...equipmentByClass,
+          armor: 'None due to Berserker',
+        }
+        armorColor = 'red'
+      }
+    }
+    // Monk
+    if (spellList.class === 'Monk') {
+      if (isMedium) {
+        equipmentByClass = {
+          ...equipmentByClass,
+          armor: 'None due to Medium',
+          weapons: 'No Great Weapons due to Medium',
+        }
+        armorColor = 'red'
+        weaponsColor = 'red'
+      }
+      if (isMystic) {
+        equipmentByClass = {
+          ...equipmentByClass,
+          weapons: 'No Great Weapons nor Heavy Thrown due to Mystic',
+        }
+        weaponsColor = 'red'
+      }
+    }
+    // Scout
+    if (spellList.class === 'Scout') {
+      if (isHunter) {
+        equipmentByClass = {
+          ...equipmentByClass,
+          shields: 'None due to Hunter',
+          weapons: 'May wield Great Weapons and Javalins due to Hunter',
+        }
+        shieldsColor = 'red'
+        weaponsColor = 'green'
+      }
+    }
+    // Warrior
+    if (spellList.class === 'Warrior') {
+      if (isMarauder) {
+        equipmentByClass = {
+          ...equipmentByClass,
+          armor: '4pts',
+          shields: 'Not Large Shields due to Marauder',
+        }
+        armorColor = 'red'
+        shieldsColor = 'red'  
+      }
+    }
+
+    return {
+      ...equipmentByClass,
+      lookThePartColor,
+      armorColor,
+      shieldsColor,
+      weaponsColor,
+    }
+  }
+
+  const isSpellTaken = (spellList: SpellList, spellId: number): boolean =>
+    spellList.spells.some(level =>
+      level.spells.some(spell => spell.id === spellId)
+  )
 
   const fetchSpellFrequency = (spellId: number, subclassSpells?: any[]) => {
     let spellDetails
@@ -169,33 +377,16 @@ function SpellListDetails() {
     let range = allSpell?.range || ''
 
     // Archetype checks
-    const isInfernal = spellList.spells.some(level =>
-      level.spells.some(baseObj => 
-        baseObj.optionalPickOne && baseObj.optionalPickOne.some(
-          spell => spell.id === 85 && spell.chosen === true)
-    ))
 
-    const isWarder = spellList.spells.some(level =>
-      level.spells.some(spell => spell.id === 171)
-    )
-    const isNecromancer = spellList.spells.some(level =>
-      level.spells.some(spell => spell.id === 104)
-    )
-    const isWarlock = spellList.spells.some(level =>
-      level.spells.some(spell => spell.id === 172)
-    )
-    const isSummoner = spellList.spells.some(level =>
-      level.spells.some(spell => spell.id === 155)
-    )
-    const isDervish = spellList.spells.some(level =>
-      level.spells.some(spell => spell.id === 40)
-    )
-    const isLegend = spellList.spells.some(level =>
-      level.spells.some(spell => spell.id === 91)
-    )
-    const hasExtention = spellList.spells.some(level =>
-      level.spells.some(spell => spell.id === 58)
-    )
+    // Change frequency for martial classes too!
+
+    const isWarder = isSpellTaken(spellList, 171)
+    const isNecromancer = isSpellTaken(spellList, 104)
+    const isWarlock = isSpellTaken(spellList, 172)
+    const isSummoner = isSpellTaken(spellList, 155)
+    const isDervish = isSpellTaken(spellList, 40)
+    const isLegend = isSpellTaken(spellList, 91)
+    const hasExtention = isSpellTaken(spellList, 58)
 
     let frequency = ''
     const freq = spellDetails?.frequency
@@ -211,8 +402,17 @@ function SpellListDetails() {
       allSpell.name &&
       allSpell.name === 'Flame Blade'
     ) {
-      frequency += (frequency ? ' ' : '') + 'Charge x5'
-      // range = 'Self'
+      charge = 'Charge x5'
+      range = 'Self'
+    }
+    if (
+      isCorruptor &&
+      allSpell &&
+      freq && (freq.charge === null || freq.charge === undefined) &&
+      allSpell.name &&
+      allSpell.name === 'Terror'
+    ) {
+      charge = 'Charge x10'
     }
     if (
       isWarder &&
@@ -378,7 +578,7 @@ function SpellListDetails() {
     if (isOptional) {
       const chosenSpell = spellArr.find(spell => spell.chosen)
       if (chosenSpell) {
-        const chosenName = fetchSpellDetails('name', chosenSpell.id) || 'Unknown Spell'
+        const chosenName = fetchSpellDetails('name', chosenSpell.id) || ''
         const spellType = fetchSpellDetails('type', chosenSpell.id)
         const spellSchool = fetchSpellDetails('school', chosenSpell.id)
         const spellIncantation = fetchSpellDetails('incantation', chosenSpell.id)
@@ -447,18 +647,29 @@ function SpellListDetails() {
                   const spellIncantation = fetchSpellDetails('incantation', sub.id)
                   const spellMaterials = fetchSpellDetails('materials', sub.id)
                   const spellFrequency = fetchSpellFrequency(sub.id, subclassSpells)
+                  const spellMagical = fetchSubclassSpellDetails('magical', sub.id, chosenName)
+                  const spellAmbulant = fetchSubclassSpellDetails('ambulant', sub.id, chosenName)
+                  const spellExtraordinary = fetchSubclassSpellDetails('extraordinary', sub.id, chosenName)
+                  const spellTrait = fetchSubclassSpellDetails('trait', sub.id, chosenName)
+                  const spellSwift = fetchSubclassSpellDetails('swift', sub.id, chosenName)
 
                   return (
                     <Row key={i} className="ms-1">
                       <span style={{ color: 'green' }}>
-                        <span style={{ textDecoration: 'underline' }}>{fetchSpellDetails('name', sub.id) || 'Unknown Spell'}</span>
+                        <span style={{ textDecoration: 'underline' }}>{fetchSpellDetails('name', sub.id) || ''}</span>
                         {' '}{spellFrequency.frequency}
+                        {' '}{spellTrait ? '( T )' : ''}
+                        {' '}{spellMagical ? '(m)' : ''}
+                        {' '}{spellAmbulant ? '(Ambulant)' : ''}
+                        {' '}{spellExtraordinary ? '(ex)' : ''}
+                        {' '}{spellSwift ? '(Swift)' : ''}
                         {showTypeAndSchool && (
                           <>
                             {' '}
-                            ({spellType}
-                            {spellSchool ? `, ${spellSchool}` : ''}
-                            )
+                            {showTypeAndSchool && <span>( {spellType} )</span>}
+                            {spellSchool && showTypeAndSchool && (
+                              <span>( {spellSchool} )</span>
+                            )}
                           </>
                         )}
                       </span>
@@ -507,7 +718,7 @@ function SpellListDetails() {
             </Row>
             {spellArr.map((spell, idx) => (
               <Row key={idx} className="m-0">
-                <span className="ms-3">{fetchSpellDetails('name', spell.id) || 'Unknown Spell'}</span>
+                <span className="ms-3">{fetchSpellDetails('name', spell.id) || ''}</span>
               </Row>
             ))}
           </>
@@ -689,10 +900,37 @@ function SpellListDetails() {
         )}
         {isMartialClass ? (
           <>
-            <Form.Text className='my-0'>Look the part: {spellList.lookThePart ? equipmentByClass?.lookThePart : ' - '}</Form.Text>
-            <Form.Text className='my-0'>Armor: {equipmentByClass.armor}</Form.Text>
-            <Form.Text className='my-0'>Shields: {equipmentByClass.shields}</Form.Text>
-            <Form.Text className='my-0'>Weapons: {equipmentByClass.weapons}</Form.Text>
+          {(() => {
+            const equipment = fetchEquipmentChanges(spellList)
+            return (
+              <>
+                <Form.Text className='my-0'>
+                  Look the part: 
+                  <span style={equipment.lookThePartColor ? { color: equipment.lookThePartColor } : {}}>
+                    {' '}{spellList.lookThePart ? equipment.lookThePart : ' - '}
+                  </span>
+                </Form.Text>
+                <Form.Text className='my-0'>
+                  Armor: 
+                  <span style={equipment.armorColor ? { color: equipment.armorColor } : {}}>
+                    {' '}{equipment.armor}
+                  </span>
+                </Form.Text>
+                <Form.Text className='my-0'>
+                  Shields: 
+                  <span style={equipment.shieldsColor ? { color: equipment.shieldsColor } : {}}>
+                    {' '}{equipment.shields}
+                  </span>
+                </Form.Text>
+                <Form.Text className='my-0'>
+                  Weapons: 
+                  <span style={equipment.weaponsColor ? { color: equipment.weaponsColor } : {}}>
+                    {' '}{equipment.weapons}
+                  </span>
+                </Form.Text>
+              </>
+            )
+          })()}
           </>
         ) : (
           <Form.Text>**** Spells ****</Form.Text>
