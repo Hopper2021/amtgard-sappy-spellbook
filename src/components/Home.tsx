2f8a64e181bae0c428135d3af7189a17e68d80c1
Hopper2021 <jessica.a.buckwalter@gmail.com>
2025-10-06 00:11:01 -0500

    Fix ternary condition

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -22,7 +22,6 @@ function App() {
   }
   const [selectedSpellList, setSelectedSpellList] = useState(emptySpellList)
   const [longPressTimeout, setLongPressTimeout] = useState<NodeJS.Timeout | null>(null)
-  const newestVersions = CURRENT_AMTGARD_VERSION || "8.6.4"
 
   let enableTips = localStorage.getItem('enableTips')
   // let theme = localStorage.getItem('theme')
@@ -154,7 +153,7 @@ function App() {
                   if (spellList.version === "V8.6.4") {
                     updateOldVersionedSpellList(spellList)
                   }
-                  spellList.version && spellList.version === newestVersions // needs this when updating 8.6.4 to 8.7.
+                  spellList.version && spellList.version === (CURRENT_AMTGARD_VERSION || "8.6.4") // needs this when updating 8.6.4 to 8.7.
                   ? navigate(`/listDetails/${spellList.id}`)
                   : navigate(`/legacyListDetails/${spellList.id}`)
                 }}
